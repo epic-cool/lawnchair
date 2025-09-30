3107df01bf25a8f3b19ddab3b6a4ecfedb2f4941
epiccool <wwas03610@gmail.com>
2025-09-30 21:24:35 +0100

    w

---
+++ src/main/java/net/epiccool/lawnchair/Lawnchair.java
@@ -0,0 +1,15 @@
+package net.epiccool.lawnchair;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lawnchair implements ModInitializer {
+	public static final String MOD_ID = "lawnchair";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/epiccool/lawnchair/LawnchairClient.java
@@ -0,0 +1,10 @@
+package net.epiccool.lawnchair;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LawnchairClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
